6e2771c04fe2c8e5c19884eba5d19c69d2358b0e
HIBERUS\jgortega <programandorioja@gmail.com>
2024-04-18 13:58:22 +0200

    Empleado con errores de compilacion

---
+++ src/main/java/com/employeeManagerApp/Repo/EmployeeRepo.java
@@ -0,0 +1,15 @@
+package com.employeeManagerApp.Repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.employeeManagerApp.employee.model.Employee;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    
+	void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
